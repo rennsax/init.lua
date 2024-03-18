b6105757bbf1687af5792a5041ef44e0290373f7
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-18 14:46:07 +0800

    feat(ui): indent-blankline.nvim, bufferline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -50,6 +50,98 @@ local ui_plugins = {
     end
   },
 
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
+    opts = {
+      options = {
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        middle_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        right_mouse_command = nil,
+        always_show_bufferline = true,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "Explorer",
+            highlight = "Directory",
+            text_aligh = "left",
+          }
+        }
+      }
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+    dependencies = {
+      -- buffer remove
+      {
+        "echasnovski/mini.bufremove",
+        keys = {
+          {
+            "<leader>td",
+            function()
+              local bd = require("mini.bufremove").delete
+              if vim.bo.modified then
+                local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+                if choice == 1 then -- Yes
+                  vim.cmd.write()
+                  bd(0)
+                elseif choice == 2 then -- No
+                  bd(0, true)
+                end
+              else
+                bd(0)
+              end
+            end,
+            desc = "[T]ab [D]elete (replace :bd)",
+          },
+          -- stylua: ignore
+          { "<leader>tD", function() require("mini.bufremove").delete(0, true) end, desc = "Forcely [T]ab [D]elete (replace :bD)" },
+        },
+      },
+    }
+  },
+
 }
 
 return ui_plugins
