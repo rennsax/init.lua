9ae46ad0fef19efaebefa502d379a85ae773bb29
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-21 00:27:09 +0800

    feat(git): git integration with gitsigns
    Add more key mappings.

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -7,4 +7,25 @@ return {
     changedelete = { text = "~" },
     untracked = { text = "│" },
   },
+  on_attach = function(buffer)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, desc)
+      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+    end
+
+    -- stylua: ignore start
+    map("n", "]h", gs.next_hunk, "Next Hunk")
+    map("n", "[h", gs.prev_hunk, "Prev Hunk")
+    map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "[G]it [H]unk [S]tage")
+    map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "[G]it [H]unk [R]eset")
+    map("n", "<leader>ghu", gs.undo_stage_hunk, "[G]it [H]unk [U]ndo")
+    map("n", "<leader>ghS", gs.stage_buffer, "[G]it Buffer [S]tage")
+    map("n", "<leader>ghR", gs.reset_buffer, "[G]it Buffer [R]eset")
+    map("n", "<leader>ghp", gs.preview_hunk_inline, "[G]it [H]unk [P]review")
+    map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "[G]it Line [B]lame")
+    map("n", "<leader>gd", gs.diffthis, "[G]it [D]iff")
+    map("n", "<leader>gD", function() gs.diffthis("~") end, "[G]it [D]iff HEAD~")
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "git hunk")
+  end
 }
