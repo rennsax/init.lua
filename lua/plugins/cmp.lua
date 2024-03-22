7e1eed637aa140a1e0e681241dd9c7d2ef703938
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-22 18:16:20 +0800

    feat(cmp): completion settings
    UI, snippets, ...

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,13 +3,29 @@ if vim.g.vscode then
 end
 
 -- Options must be wrapped in a callback since it requires some modules.
-local get_nvim_cmp_options = function()
+local cmp_options = function()
   local cmp = require "cmp"
   local luasnip = require "luasnip"
+  local defaults = require("cmp.config.default")()
 
   local options = {
+
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
+
     completion = {
       completeopt = "menu,menuone,noinsert",
+    },
+
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+      -- completion = {
+      --   scrollbar = false,
+      -- },
     },
 
     -- The order of the sources determines their order in the completion results.
@@ -38,6 +54,13 @@ local get_nvim_cmp_options = function()
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-p>"] = cmp.mapping.select_prev_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-c>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.abort()
+        else
+          fallback()
+        end
+      end),
       ["<CR>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(1) then
           luasnip.jump(1)
@@ -53,6 +76,20 @@ local get_nvim_cmp_options = function()
         end
       end, {'i','s'}),
     },
+
+    formatting = {
+      format = function(_, item)
+        local icons = require("lazyvim.config").icons.kinds
+        if icons[item.kind] then
+          item.kind = icons[item.kind] .. item.kind
+        end
+        return item
+      end,
+    },
+    -- performance = {
+    --   max_view_entries = 20,
+    -- },
+    sorting = defaults.sorting,
   }
   return options
 end
@@ -105,7 +142,7 @@ return {
         "hrsh7th/cmp-path",
       }
     },
-    opts = get_nvim_cmp_options,
+    opts = cmp_options,
     config = function(_, opts)
       local lsp_zero = require("lsp-zero")
       lsp_zero.extend_cmp()
