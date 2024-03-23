6ada06fe971d0214d83408349aa35b7f7bb59f63
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-23 18:39:45 +0800

    refactor(lazy): move lazy init scripts to configs.lazy

--- init.lua
+++ init.lua
@@ -1,27 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
-
-vim.opt.rtp:prepend(lazypath)
-
 require "configs.options"
 
--- load plugins
--- For lazy.nvim, specify either `opts = {}` or `config = true` will run
--- the default setup routine (`require(MAIN).setup()`) when the plugin is loaded.
-require("lazy").setup(vim.tbl_extend("error", require("plugins.configs.lazy"), {
-  spec = {
-    { import = "plugins.extras.vscode" },
-    { import = "plugins" },
-  },
-}))
+require "configs.lazy"
 
 vim.schedule(function()
   require "configs.mappings"
--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,4 +1,21 @@
-return {
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
+-- For lazy.nvim, specify either `opts = {}` or `config = true` will run
+-- the default setup routine (`require(MAIN).setup()`) when the plugin is loaded.
+require("lazy").setup({
+  spec = {
+    { import = "plugins.extras.vscode" },
+    { import = "plugins" },
+  },
   defaults = { lazy = true },
   install = {
     missing = true,
@@ -47,4 +64,5 @@ return {
       },
     },
   },
-}
+})
+
