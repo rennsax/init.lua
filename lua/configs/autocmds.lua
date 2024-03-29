d78feb96bac5e4516e8c608dbdb8f53262390656
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-29 15:01:21 +0800

    feat(autocmds): update my autocmds
    Most of which is copied from lazyvim.

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -1,13 +1,59 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("vim-augrp-" .. name, { clear = true })
+end
+
 -- Highlight when yanking (copying) text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = augroup('highlight-yank'),
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last-location"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf]._last_loc then
+      return
+    end
+    vim.b[buf]._last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto-create-dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "Dirchanged", "BufRead", "BufNewFile" }, {
+  group = augroup("project-level-settings"),
+  callback = function()
+    -- See :help file-searching for why a ';' is appended (upward search).
+    local project_setting = vim.fn.findfile(".vim/init.lua", vim.fn.expand('%:p')..';')
+    if vim.fn.filereadable(project_setting) ~= 0 then
+      vim.cmd("source " .. project_setting)
+    end
+  end,
+  desc = "Automatically search the closest .vim folder and source the init.lua within",
 })
 
